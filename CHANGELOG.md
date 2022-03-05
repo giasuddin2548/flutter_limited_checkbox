0db39ae1905574e627c012fd77e2450d8dcd620a
Gias Uddin Samir <giasuddin2548@gmail.com>
2022-03-05 17:21:37 +0600

    example deleted

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## 0.0.3
+- Readme updated
+
+## 0.0.2
+- Demo Added
+- Description added
+
 ## 0.0.1
 
 CheckBox Limited, Unlimited, single selection added
--- example/android/app/src/main/kotlin/com/smart_select/smart_select_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.smart_select.smart_select_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
